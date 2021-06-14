9638776e1ce73a5d8675f73c4bbd980694acd26e
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-06-14 16:22:07 +0530

    Connecting to DB

---
+++ config/db.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  //   mongoose
+  //     .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  //     .then(() => console.log('MongoDB connected'))
+  //     .catch(err => {
+  //       console.log(err);
+  //       process.exit(1);
+  //     });
+
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
