e1f540b999b26d24267fcd4477924aa13843420a
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-06-15 14:52:41 +0530

    Routing setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,23 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NavBar from './components/layout/NavBar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
 
 const App = () => {
-  return <div className='App'>My App</div>;
+  return (
+    <Router>
+      <>
+        <NavBar />
+        <div className='container'>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/about' component={About} />
+          </Switch>
+        </div>
+      </>
+    </Router>
+  );
 };
 
 export default App;
