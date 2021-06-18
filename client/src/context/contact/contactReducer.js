be0de3ac7698797f53badec3c5d4de678ca6ac1b
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-06-18 22:53:08 +0530

    Delete contacts fixes

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -22,7 +22,7 @@ export const reducer = (state, action) => {
     case ADD_CONTACT:
       return {
         ...state,
-        contacts: [...state.contacts, action.payload],
+        contacts: [action.payload, ...state.contacts],
         loading: false,
       };
     case UPDATE_CONTACT:
