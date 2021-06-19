4bb6681cd8facf1001a33d1b946641adaae8c11e
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-06-19 10:46:07 +0530

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -17,6 +18,16 @@ app.use(cors());
 
 // Routes
 app.use('/api', require('./routes'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
