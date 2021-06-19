416da1f770136e1e5664ffe2932a5f512e37de28
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-06-19 13:40:34 +0530

    more deployment changes

--- server.js
+++ server.js
@@ -14,20 +14,23 @@ app.use(express.json({ extended: false }));
 app.use(cors());
 
 /* Don't uncomment below line in preoduction */
-// app.get('/', (req, res) => res.json({ msg: `Welcome to contact keeper API` }));
+app.get('/', (req, res) => res.json({ msg: `Welcome to contact keeper API` }));
 
 // Routes
 app.use('/api', require('./routes'));
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
 
-  app('/*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  app(
+    '/*',
+    (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')),
+    next()
   );
 }
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
