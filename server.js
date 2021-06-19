f4a3f21c67dd6b3dfa446eef151c54ce5df648d5
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-06-19 13:47:27 +0530

    more deployment changes

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json({ extended: false }));
 app.use(cors());
 
 /* Don't uncomment below line in preoduction */
-app.get('/', (req, res) => res.json({ msg: `Welcome to contact keeper API` }));
+// app.get('/', (req, res) => res.json({ msg: `Welcome to contact keeper API` }));
 
 // Routes
 app.use('/api', require('./routes'));
@@ -24,11 +24,8 @@ if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
 
-  app.get(
-    '/*',
-    (req, res) =>
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')),
-    next()
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
 }
 
